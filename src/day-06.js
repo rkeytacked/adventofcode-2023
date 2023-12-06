671e5c2ea8ea11aaefb25cd406b4d8e55b993997
Peter Liske <peter.liske@gmx.net>
2023-12-06 06:46:35 +0100

    added Day 06

---
+++ src/day-06.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+const {log, map, median, min, max, readCharArrays, readLines, readSingletonMaps, sort, split, sum, prod, toNumber, spanYX, unique} = require("./common");
+
+/* * * * * * * *
+ * * DAY  06 * *
+ * * * * * * * */
+
+const [times, distances] = readLines('../inputs/06.txt', split(/\s+/))
+    .map(([, ...numbers]) => numbers.map(toNumber));
+
+/* * * * * * * *
+ * * Part #1 * *
+ * * * * * * * */
+
+const nullDistance = (t, d) => {
+    const term = (t / 2) * (t / 2) - d;
+    if (term <= 0) {
+        return 0;
+    }
+    const root = Math.sqrt(term);
+    const a = Math.floor(t / 2 - root + 1);
+    const b = Math.ceil(t / 2 + root - 1);
+    return b - a + 1;
+}
+
+const wins = times.map((time, i) => nullDistance(time, distances[i]));
+
+log('solution #1: ', prod(wins));
+
+log('\n-----------------------------------------------------------\n')
+
+/* * * * * * * *
+ * * Part #2 * *
+ * * * * * * * */
+
+let bigTime = Number(times.join(''));
+let bigDistance = Number(distances.join(''));
+
+log('solution #2: ', nullDistance(bigTime, bigDistance));
